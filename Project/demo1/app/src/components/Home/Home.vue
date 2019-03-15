82abaad53c82637e32329bec857baae1d71306d4
YangMr <759168442@qq.com>
2019-03-15 17:44:58 +0800

    增加了部分代码

--- Project/demo1/app/src/components/Home/Home.vue
+++ Project/demo1/app/src/components/Home/Home.vue
@@ -2,15 +2,35 @@
     <div>
       <app-swiper></app-swiper>
       <app-infobtn></app-infobtn>
-      <div style="height:800px">我想好好许欸小</div>
+      <button @click="post">获取后台提供的数据</button>
     </div>
 </template>
 
 <script>
   import Swiper from '../common/Swiper'
   import Infobtn from '../common/Infobtn'
+  let data = {id : "1"};
+
     export default {
       name: "Home",
+      methods : {
+        post : function () {
+          this.$axios({
+            method : "post",
+            url : "http://234j08j718.iok.la/proxy_manage/app/getStudentById3",
+            data : this.$qs.stringify(data),
+          }).then(response=>{
+            console.log(response)
+          },error=>{
+            console.log(`错误信息:${error.message}`)
+          })
+          // this.$axios.post("http://234j08j718.iok.la/proxy_manage/app/getStudentById3",{id : "1"}).then(response=>{
+          //   console.log(response)
+          // },error=>{
+          //   console.log(`错误信息:${error.message}`)
+          // })
+        }
+      },
       components : {
         appSwiper :Swiper,
         appInfobtn :Infobtn
@@ -23,5 +43,10 @@
 </script>
 
 <style scoped>
-
+  button{
+    display: block;
+    width: 160px;
+    margin:20px auto;
+    height:55px;
+  }
 </style>
--- Project/demo1/app/src/main.js
+++ Project/demo1/app/src/main.js
@@ -9,6 +9,12 @@ import 'mint-ui/lib/style.css'
 Vue.use(MintUI);
 import Axios from 'axios'
 Vue.prototype.$axios = Axios;
+//引入qs模块
+import qs from 'qs';
+//将qs模块挂载到vue的原型上,在全局就可以进行使用
+Vue.prototype.$qs = qs;
+//设置请求的头信息
+Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 import {store} from './store/store'
 Vue.config.productionTip = false
 /* eslint-disable no-new */
